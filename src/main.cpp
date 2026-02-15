78921e21d003495cb3de095ac06d1296fd383854
gersastas <gersastas@gmail.com>
2026-02-15 13:26:35 +0300

    feat(main): add project skeleton and logging setup

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,54 @@
  */
 
 #include <iostream>
+
+
+/**
+ * \file main.cpp
+ * \brief Точка входа в приложение csv_median_calculator.
+ *
+ * Обеспечивает инициализацию логирования, парсинг аргументов командной
+ * строки, оркестрацию процесса обработки данных и вывод результатов.
+ */
+
+#include "config_parser.hpp"
+#include "csv_reader.hpp"
+#include "file_scanner.hpp"
+#include "median_calculator.hpp"
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
+#include <boost/program_options.hpp>
+
+#include <chrono>
+#include <filesystem>
+#include <iostream>
+#include <optional>
+#include <string>
+
+namespace po = boost::program_options;
+
+/**
+ * \brief Настраивает систему логирования (spdlog).
+ *
+ * Использует цветной вывод в консоль с установленным форматом сообщений.
+ */
+void setup_logging() {
+    auto console_sink =
+        std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    auto logger = std::make_shared<spdlog::logger>("main", console_sink);
+
+    spdlog::set_default_logger(logger);
+    spdlog::set_level(spdlog::level::info);
+    // Формат: [Дата Время] [Уровень] Сообщение
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] %v");
+}
+
+/**
+ * \brief Основная функция приложения.
+ */
+
 int main() {
     std::cout << "CSV Median Calculator" << std::endl;
 }
